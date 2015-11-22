00a7ab1fa8e800e2ed9a7efbebcb11fa9de2e61b
gokahanabi <gokahanabi@sanayiTipi>
2015-11-22 02:58:13 +0200

    q8 faillo.

---
+++ peuler/src/main/java/peuler/question/Q8.java
@@ -0,0 +1,40 @@
+package peuler.question;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Q8 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int tcase = scanner.nextInt();
+		List<Integer> digits = new ArrayList<>();
+		List<String> inputs = new ArrayList<>();
+		for (int i = 0; i < tcase; i++) {
+			scanner.nextLine();
+			digits.add(Integer.parseInt(scanner.nextLine().split(" ")[1]));
+			inputs.add(scanner.next());
+		}
+		for (int i = 0; i < tcase; i++) {
+			findLargestConsecDigitProduct(digits.get(i), inputs.get(i));
+		}
+		scanner.close();
+	}
+
+	private static void findLargestConsecDigitProduct(int consc, String num) {
+		int max = 0;
+		for (int i = 0; i < num.length() - consc; i += consc) {
+			int product = 1;
+			for (int j = i + 1; j < i + 1 + consc; j++) {
+				product *= Integer.parseInt(num.substring(j, j + 1));
+				if (product == 0)
+					break;
+			}
+			if (product > max)
+				max = product;
+		}
+		System.out.println(max);
+	}
+
+}
